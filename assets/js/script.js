3b96f45a129b3ec0d1b1b8151fe8475023676924
M Amiruddin Irsyad <amier.dev@gmail.com>
2024-08-03 12:00:51 +0700

    slicing hero - testimonial

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// Variable
+// 1. Var = Old School
+var x = 0
+x = 15
+
+console.log(x)
+
+// Variable modern
+// 1. let
+let z = 10
+z = 5
+// let 0 // tidak boleh pake angka
+let z0 = 10
+let _a = test
+let $a = 10
+// let a* = 10
+let Z = 15
+let isCarBroken = true
+
+console.log(z)
+// 2. const
+const COLOR_RED = "red";
+const y = 5
+y = 10
+
+console.log(y)
